bdc8f47ecec8b08db42f47d45324f044338ad306
13472889991 <haoming.lim@indiansprings.org>
2018-03-26 08:06:36 -0500

    Add files via upload

--- scheduling.py
+++ scheduling.py
@@ -9,6 +9,7 @@ class Task():
     
     def __str__(self):
         return (str(self.start)+ " : " +str(self.end))
+    
 def schedule(tasks):
     ''' Takes Tasks, a list of Task objects sorted by finishing time, and returns
         the optimal amount of scheduled tasks in order '''
@@ -18,4 +19,20 @@ def schedule(tasks):
         if task.start >= currentEnd:
             output.append(task)
             currentEnd = task.end
-    return (output)
+    return (output[-1])
+
+def compatible(task, tasks):
+    ''' Given a task, and a separate list of tasks, returns a list of tasks that
+        are compatible with the given task '''
+    output = []
+    for i in tasks:
+        if i.end <= task.start
+            output.append(i)
+    return output
+
+def weightedSchedule(tasks):
+    opt=[0 for i in range(1,len(tasks))
+    for i in range(1,len(tasks)):
+        opt[i] = (max(i.weight + opt[compatible(i), opt[i-1]))
+    return opt
+                  
